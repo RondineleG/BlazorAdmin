86a22ffffb9be2e1a9dc0057f0aba4765d3f1e8b
Rondinele Guimaraes <rondineleg@edu.univali.br>
2019-11-20 15:26:26 -0300

    Remove 'using' not used.

--- BlazorAdmin2/Startup.cs
+++ BlazorAdmin2/Startup.cs
@@ -1,20 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlazorAdmin2.Areas.Identity;
+using BlazorAdmin2.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorAdmin2.Areas.Identity;
-using BlazorAdmin2.Data;
 
 namespace BlazorAdmin2
 {
